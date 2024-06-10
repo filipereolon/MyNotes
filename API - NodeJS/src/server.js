2e4ca19ab72bb7efd98ccd706e6fa780ccb04499
filipereolon <filipereolon@gmail.com>
2024-06-10 08:59:13 -0300

    Install and configuration of p2m

--- API - NodeJS/src/server.js
+++ API - NodeJS/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 require('express-async-errors')
 
 const migrationsRun = require('./Database/Sqlite/Migrations')
@@ -27,5 +28,5 @@ app.use((err, req, res, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
